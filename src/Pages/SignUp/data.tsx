036920dd4b14a2b46746c137b43b06e4bc5c72f3
Finn Mao <loverainys@gmail.com>
2023-12-13 17:18:47 +0800

    feat: add Signup page

---
+++ src/Pages/SignUp/data.tsx
@@ -0,0 +1,43 @@
+export type Data = {
+  componentName: string;
+  labelFor: string;
+  title: string;
+  inputType: string;
+  inputName: string;
+  placeholder: string;
+};
+
+export const dataset: Data[] = [
+  {
+    componentName: "Email",
+    labelFor: "email",
+    title: "Email",
+    inputType: "email",
+    inputName: "email",
+    placeholder: "請輸入Email",
+  },
+  {
+    componentName: "UserName",
+    labelFor: "username",
+    title: "您的暱稱",
+    inputType: "text",
+    inputName: "username",
+    placeholder: "請輸入暱稱",
+  },
+  {
+    componentName: "Password",
+    labelFor: "password",
+    title: "密碼",
+    inputType: "password",
+    inputName: "password",
+    placeholder: "請輸入密碼",
+  },
+  {
+    componentName: "CheckPassword",
+    labelFor: "password",
+    title: "再次輸入密碼",
+    inputType: "password",
+    inputName: "password",
+    placeholder: "請再次輸入密碼",
+  },
+];
--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,5 +1,68 @@
+import { useNavigate } from "react-router-dom";
+import { Data, dataset } from "./data";
+import {
+  Wrapper,
+  Title,
+  Form,
+  Input,
+  Email,
+  UserName,
+  Password,
+  CheckPassword,
+  Container,
+  Button,
+  Login,
+} from "./styled";
+
 const SignUp = () => {
-  return <>Sign up page</>;
+  const navigate = useNavigate();
+  // 動態生成元件
+  const componentMap: { [key: string]: React.ComponentType<any> } = {
+    Email,
+    UserName,
+    Password,
+    CheckPassword,
+  };
+
+  return (
+    <Wrapper>
+      <Title>註冊帳號</Title>
+      <Form>
+        {dataset.map(
+          (
+            {
+              componentName,
+              labelFor,
+              title,
+              inputType,
+              inputName,
+              placeholder,
+            }: Data,
+            index: number
+          ) => {
+            const Component = componentMap[componentName];
+            if (!Component) {
+              return null;
+            }
+            return (
+              <Component key={index}>
+                <label htmlFor={labelFor}>{title}</label>
+                <Input
+                  type={inputType}
+                  name={inputName}
+                  placeholder={placeholder}
+                />
+              </Component>
+            );
+          }
+        )}
+      </Form>
+      <Container>
+        <Button type="button">註冊帳號</Button>
+        <Login onClick={() => navigate(-1)}>登入</Login>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default SignUp;
